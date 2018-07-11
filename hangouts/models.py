d8ca0cdcbb8fccb8f8cce9cc5fe8ef952b783b84
aghniaprawira <aghnia@staff.gramedia.com>
2018-07-11 14:54:11 +0700

    new branch

--- hangouts/models.py
+++ hangouts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class HangoutsSpace(models.Model):
+    name = models.CharField(max_length=30)
+
+class VstsArea(models.Model):
+    name = models.CharField(max_length=30)
+    hangoutsSpaces = models.ManyToManyField(HangoutsSpace)
