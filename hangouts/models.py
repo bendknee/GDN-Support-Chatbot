4aa3512a2b56aac6b39567224a58c91c172c1348
aghniaprawira <aghnia@staff.gramedia.com>
2018-07-23 15:01:29 +0700

    delete default

--- hangouts/models.py
+++ hangouts/models.py
@@ -18,7 +18,7 @@ class SoftwareSupport(WorkItem):
 class User(models.Model):
     name = models.CharField(max_length=40)
     work_item = models.ForeignKey(WorkItem, on_delete=models.CASCADE, null=True)
-    state = models.CharField(default='initial', max_length=30)
+    state = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
