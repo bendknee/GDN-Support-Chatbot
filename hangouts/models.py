ac7fa2092900ba0f9d06050a3e53ab51b0f44995
aghniaprawira <aghnia@staff.gramedia.com>
2018-07-24 15:02:37 +0700

    severity

--- hangouts/models.py
+++ hangouts/models.py
@@ -4,18 +4,22 @@ from django.db import models
 class WorkItem(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
-    path_dict = {"title": "System.Title", "description": "System.Description", "severity": "Microsoft.VSTS.Common.Severity"}
+    path_dict = {"title": "System.Title", "description": "System.Description"}
 
 
 class HardwareSupport(WorkItem):
     hardware_type = models.CharField(max_length=30)
-    path_dict = dict(WorkItem.path_dict, **{"hardware_type": "Support.HardwareType"})
+    severity = models.CharField(max_length=10)
+    self_dict = {"hardware_type": "Support.HardwareType", "severity": "Microsoft.VSTS.Common.Severity"}
+    path_dict = dict(WorkItem.path_dict, **self_dict)
 
 
 class SoftwareSupport(WorkItem):
     requested_by = models.CharField(max_length=30)
     third_party = models.CharField(max_length=30)
-    # path_dict = dict(WorkItem.path_dict, **{"requested_by": "Ticketing.HardwareType"})
+    severity = models.CharField(max_length=10)
+    self_dict = {"requested_by": "Ticketing.HardwareType", "severity": "Microsoft.VSTS.Common.Severity"}
+    # path_dict = dict(WorkItem.path_dict, **self_dict)
 
 
 class User(models.Model):
