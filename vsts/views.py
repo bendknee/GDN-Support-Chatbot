2e1bb1ee9bff21dfbfc3b8e607a3e228d6cb7724
aghniaprawira <aghnia@staff.gramedia.com>
2018-08-06 12:24:47 +0700

    debug

--- vsts/views.py
+++ vsts/views.py
@@ -34,6 +34,7 @@ def create_work_item(work_item_dict, url, user):
         payload.append(field)
 
     req = requests.post(url, headers=headers, data=json.dumps(payload))
+    print(req.json())
 
     CreatedWorkItems.objects.create(id=req.json()['id'], user=user)
 
