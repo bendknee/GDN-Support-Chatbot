fec7aed6e23f42a8431728b3402acaefb5620174
aghniaprawira <aghnia@staff.gramedia.com>
2018-08-14 14:11:10 +0700

    notification message

--- vsts/views.py
+++ vsts/views.py
@@ -50,8 +50,8 @@ def notification(request):
 
         work_item = CreatedWorkItems.objects.get(id=event['resource']['workItemId'])
 
-        send_message("NOTIFICATION")
-        send_message("Your Work Item has been updated:")
+        send_message("NOTIFICATION", work_item.user.name)
+        send_message("Your Work Item has been updated:", work_item.user.name)
         send_message(body, work_item.user.name)
 
         return JsonResponse({"text": "success!"}, content_type='application/json')
